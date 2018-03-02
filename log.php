c0c926c4cf04017eb356e563afd337904f08b008
bwe2 <bwe2@njit.edu>
2018-03-02 18:43:34 -0500

    idk where this will appear

--- log.php
+++ log.php
@@ -1,5 +1,8 @@
 <?php
 $epoch = time();
 echo gmdate('r', $epoch);
-
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) .'/log.txt');
+error_reporting(E_ALL);
 ?>
