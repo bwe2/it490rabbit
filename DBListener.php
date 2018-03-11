4f5a16c7a8b15722158fde65c6fabdc3c52600cd
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-11 19:14:53 -0400

    Update DBListener.php

--- DBListener.php
+++ DBListener.php
@@ -32,7 +32,7 @@ function requestProcessor($request)
   switch ($request['data'])
   {
     case "login":
-      $status = doLogin($request['username'],$request['password']);
+      $status = doLogin($request['Username'],$request['Password']);
       break;
     case "register":
       $status = doRegister($request['username'],$request['password'],$request['email']);
