67bb8c784fcf11359431ed996270e005a4cbc908
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 15:26:45 -0400

    Update testRabbitMQServer.php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -5,8 +5,28 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('DBFunction.php.inc');
 echo "Server Started".PHP_EOL;
-
-//function doLogin($username,$password)
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+echo $argv[0]." END".PHP_EOL;
+/*
+function doLogin($username,$password)
 {
     // lookup username in databas
     // check password
@@ -39,4 +59,5 @@ return $response;
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 //$server->process_requests('requestProcessor');
 exit();
+*/
 ?>
