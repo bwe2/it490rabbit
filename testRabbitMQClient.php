1b20d7294d1101a4e6af857326eaaa9a2f0fac86
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 16:54:53 -0400

    Update testRabbitMQClient.php

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -4,7 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-//function createClient($request){
+
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
@@ -26,6 +26,4 @@ echo "client recieved response:one ".PHP_EOL;
 print_r($response);
 echo "\n\n";
 echo $argv[0]." END".PHP_EOL;
- //return $response;
-//}
 ?>
