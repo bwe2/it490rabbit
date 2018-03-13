e05e421474b334343d61ab57d44e84035595d3bc
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 16:14:34 -0400

    Update Loginone.php

--- Loginone.php
+++ Loginone.php
@@ -9,18 +9,16 @@ $request = array();
 $request['username'] = $_POST['username'];
 $request['password'] = $_POST['password'];
 
-//var_dump($request);
+var_dump($request);
 
 $response = createClient($request);
 
-
-/*
-//$username = $_POST['username'];
+$username = $_POST['username'];
 $password = $_POST['password']; 
 
 
 
-
+/*
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -40,6 +38,7 @@ $response = $client->send_request($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 
+*/
 //$response = $client->publish($request);
 
 
@@ -49,4 +48,5 @@ print_r($response);
 
 //echo $username;
 //echo $password;
+
 ?>
