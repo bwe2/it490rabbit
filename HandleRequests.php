e6056c3449f7cbdd2f6160b450e6d3611df433dd
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 17:49:13 -0400

    Update HandleRequests.php

--- HandleRequests.php
+++ HandleRequests.php
@@ -1,22 +1,16 @@
 <?php
 include("testRabbitMQClient.php");
-/*
+
 include("log.php");
 require_once('DBFunction.php.inc')
 require_once('path.inc');
 require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-//if post message is not set
-	$request = array();
-
-
-$request['username'] = $_POST['username'];
-$request['password'] = $_POST['password'];
-$response = createClient($request);
-$password = $_POST['password'];
-
-echo "client received response one: ".PHP_EOL;
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+//require_once('rabbitMQLib.inc');
+//$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+function doLogin($username,$password){
+	$sconnect = new ConnectDB("it490');
+	return $sconnect->validateLogin($username,$password);
+	}
 if (!isset($_POST))
 {
 	$msg = "NO POST MESSAGE SET";
@@ -28,7 +22,7 @@ $postRequest = $_POST;
 $response = "unsupported request type"; //default response
 
 echo "client received response: two ".PHP_EOL;
-switch ($postRequest["data"])
+switch ($postRequest["type"])
 {
 	case "login":
 		$password = $postRequest["password"];
@@ -52,5 +46,4 @@ echo json_encode($response);
 
 echo "client received response: three ".PHP_EOL;
 exit(0);
-*/
 ?>
