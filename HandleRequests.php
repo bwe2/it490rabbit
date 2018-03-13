ed39f86f7e9867902111f928aa6fbfee2590172a
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 19:12:19 -0400

    Update HandleRequests.php

--- HandleRequests.php
+++ HandleRequests.php
@@ -1,6 +1,6 @@
 <?php
 include("testRabbitMQClient.php");
-
+/*
 //include("log.php");
 require_once("DBFunction.php.inc")
 //require_once("path.inc");
@@ -47,4 +47,5 @@ echo json_encode($response);
 
 echo "client received response: three ".PHP_EOL;
 exit(0);
+*/
 ?>
