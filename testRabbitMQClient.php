d4f0869b1a762ca33b02d49bf464a143f182d6f8
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 18:01:16 -0400

    Update testRabbitMQClient.php

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -15,9 +15,9 @@ else
   $msg = "test message";
 }
   $request = array();
-$request['type'] = $_POST['type'];
-$request['username'] = $_POST['username'];
-$request['password'] = $_POST['password'];
+$request['type'] = "Login";
+$request['username'] = "test";
+$request['password'] = "test";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
