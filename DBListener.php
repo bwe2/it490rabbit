ce4b07b482efe1218dd5f5391715059711c8b0f0
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-13 22:41:30 -0400

    Update DBListener.php

--- DBListener.php
+++ DBListener.php
@@ -1,6 +1,39 @@
 #!/usr/bin/php
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('DBFunction.php.inc');
+echo "Server Started".PHP_EOL;
+function doLogin($username,$password)
+{
+    $sconnect = new ConnectDB("itclass");
+return $sconnect->validateLogin($username,$password);
+    return true;
+    //return false if not valid
+}
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+$server->process_requests('requestProcessor');
+exit();
 
+/*
 include("log.php");
 echo "Server Started".PHP_EOL;
 require_once('path.inc');
@@ -45,4 +78,5 @@ function requestProcessor($request)
 $server = new rabbitMQServer("testRabbitMQDB.ini","testServer");
 $server->process_requests('requestProcessor');
 exit();
+*/
 ?>
