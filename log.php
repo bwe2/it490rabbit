2bc5a12007d5be370d817468a52a2143a6782acf
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-14 19:12:12 -0400

    Update log.php

--- log.php
+++ log.php
@@ -1,5 +1,5 @@
 <?php
-function writelog($log){
+function writelog($request){
   $epoch = time();
   $date = gmdate("Y-m-d H:i:s", $epoch);
   $file = "log.txt";
@@ -7,12 +7,12 @@ function writelog($log){
   fwrite($fh, "\n" . $date . "\t" . $log);
   fclose($fh);
 }
-function writelogDB($log){
+function writelogDB($request){
   $epoch = time();
   $date = gmdate("Y-m-d H:i:s", $epoch);
   $file = "logDB.txt";
   $fh = fopen($file, 'a') or die ("Cannot open");
-  fwrite($fh, "\n" . $date . "\t" . $log);
+  fwrite($fh, "\n" . $date . "\t" . $request);
   fclose($fh);
 }
 
