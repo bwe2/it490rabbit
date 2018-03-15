fdc78b7722eee81e8ee8f56a8cd213c97efdb884
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-14 22:00:58 -0400

    Update HandleRequests.php

--- HandleRequests.php
+++ HandleRequests.php
@@ -1,18 +1,13 @@
 <?php
-include("testRabbitMQClient.php");
-/*
-//include("log.php");
-require_once("DBFunction.php.inc")
-//require_once("path.inc");
-//require_once("get_host_info.inc");
-//require_once("rabbitMQLib.inc");
-//$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-function doLogin($username,$password){
-	$sconnect = new ConnectDB("itclass");
-	return $sconnect->validateLogin($username,$password);
-	}
-if (!isset($_POST))
-{
+include('log.php');
+require_once('DBFunction.php.inc')
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+//session_start();
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+if (!isset($_POST)){
 	$msg = "NO POST MESSAGE SET";
 	writelog($msg);//write to log file
 	echo json_encode($msg);
@@ -23,9 +18,16 @@ $postRequest = $_POST;
 $response = "unsupported request type"; //default response
 
 echo "client received response: two ".PHP_EOL;
-switch ($_POST["type"])
-{
+switch ($postRequest["type"]){
 	case "login":
+		$request = array()
+		$request['type'] = $postRequest["type"];
+		$request['username'] = $postRequest["username"];
+		$request['password'] = $postRequest["password"];
+		$response = $client->send_request($request);
+		$returnarray =json_decode($response, true);
+		$response =$returnarray['message'];
+		/*
 		$login = doLogin($_POST["username"],$_POST["password"]);
 		if($login){
 			$response = "successful";
@@ -33,20 +35,22 @@ switch ($_POST["type"])
 		else{
 			$response = "failed";
 		}
-		//$password = $postRequest["password"];
-		//$hashedPass = sha1($password);//hash the pass
-		//$postRequest["password"] = $hashedPass;
-		//$response = createClient($postRequest);
+		$password = $postRequest["password"];
+		$hashedPass = sha1($password);//hash the pass
+		$postRequest["password"] = $hashedPass;
+		$response = createClient($postRequest);
+		*/
 	break;
 	case "register":
 		$password = $postRequest["password"];
 		$email = $postRequest["email"];
-		$hashedPass = password_hash($password, PASSWORD_DEFAULT);//hash the pass
-		$postRequest["password"] = $hashedPass;
-		$response = createClient($postRequest);
+		//$hashedPass = password_hash($password, PASSWORD_DEFAULT);//hash the pass
+		//$postRequest["password"] = $hashedPass;
+		//$response = createClient($postRequest);
 	break;
 	
 }
+echo $response;
 //write to log file
 writelog(json_encode($response));
 //turn the response into a JSON object
@@ -54,5 +58,10 @@ echo json_encode($response);
 
 echo "client received response: three ".PHP_EOL;
 exit(0);
+/*
+function doLogin($username,$password){
+	$sconnect = new ConnectDB("itclass");
+	return $sconnect->validateLogin($username,$password);
+	}
 */
 ?>
