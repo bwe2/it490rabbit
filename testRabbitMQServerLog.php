5fc87be5db1a569d334866d7f94d4b8bc97c1eb3
bwe2 <35790319+bwe2@users.noreply.github.com>
2018-03-15 01:09:15 -0400

    Create testRabbitMQServerLog.php

---
+++ testRabbitMQServerLog.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('logRec.inc');
+function requestProcessor($request)
+{
+  switch ($request['type']) {
+    case "error":
+      //echo "-Error Recieved-" . PHP_EOL;
+      logIt($request);
+      break;
+  }
+}
+//function end
+$server = new rabbitMQServer("testRabbitMQLog.ini", "testServer");
+$server->process_requests('requestProcessor');
+exit();
+?>
